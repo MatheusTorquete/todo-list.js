b03e2dcc7e20ef268163e25b0acb1ba00d55bb63
Matheus <matheuswe10@gmail.com>
2023-06-09 18:13:26 -0300

    primeira parte todolist

---
+++ app.js
@@ -0,0 +1,12 @@
+'use strict';
+// criação dos elementos
+const criarItem = (tarefa, status) => {
+    const item = document.createElement('label');
+    item.classList.add('todo__item');
+    item.innerHTML = `
+    <input type="checkbox ${status}>
+    <div>${tarefa}</div>
+    <input type="button" value="X"`
+
+    document.getElementById('todoList').appendChild(item);
+}
