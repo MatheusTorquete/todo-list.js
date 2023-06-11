0f867ef4a9f054a49ec6e86b8b89325dee6d8447
Matheus <matheuswe10@gmail.com>
2023-06-11 10:12:49 -0300

    segunda parte todolist

--- app.js
+++ app.js
@@ -1,12 +1,51 @@
 'use strict';
+
+let banco = [
+    {'tarefa': 'Estudar JS', 'status': ''},
+    {'tarefa': 'netflix', 'status': 'checked'}
+]
+
 // criação dos elementos
-const criarItem = (tarefa, status) => {
+const criarItem = (tarefa, status, indice) => {
     const item = document.createElement('label');
     item.classList.add('todo__item');
     item.innerHTML = `
-    <input type="checkbox ${status}>
+    <input type="checkbox" ${status} data-indice=${indice}>
     <div>${tarefa}</div>
-    <input type="button" value="X"`
+    <input type="button" value="X" data-indice=${indice}>`;
 
     document.getElementById('todoList').appendChild(item);
 }
+
+const limparTarefas = () => {
+    const todoList = document.getElementById('todoList');
+    while(todoList.firstChild) {
+        todoList.removeChild(todoList.lastChild);
+    }
+}
+
+const atualizarTela = () => {
+    limparTarefas();
+    banco.forEach( (item, indice) => criarItem(item.tarefa, item.status, indice));
+}
+
+const inserirItem = (evento) => {
+    const tecla = evento.key;
+    const texto = evento.target.value
+    //console.log(tecla)
+    if(tecla === 'Enter'){
+        banco.push({'tarefa': texto, 'status': ''})
+        atualizarTela();
+        evento.target.value = '';
+    }
+}
+
+const clickItem = (evento) => {
+    const elemento = evento.target;
+    console.log(elemento);
+}
+
+document.getElementById('newItem').addEventListener('keypress', inserirItem);
+document.getElementById('todoList').addEventListener('click', clickItem);
+
+atualizarTela();
